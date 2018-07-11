a76e5c9b3cef963e823046384cb6dedd571796e4
joannarogalska <rogalska.joasia@gmail.com>
2018-07-11 20:40:39 +0200

    Ukończenie projektu | Task 8.5

--- src/index.js
+++ src/index.js
@@ -3,19 +3,7 @@ import { render } from 'react-dom';
 import { Router, hashHistory } from 'react-router';
 import { Provider } from 'react-redux';
 import store from './store';
-import DevTools from './DevTools';
-import { getCountries } from './actions/actions-countries';
 import routes from './routes';
-
-// render(
-//     <Provider store={store}>
-//         <div>
-//             <h1>Inicjalizacja projektu</h1>
-//             <DevTools />
-//         </div>
-//     </Provider>,
-//     document.getElementById('root')
-// );
 
 render(
     <Provider store={store}>
@@ -23,5 +11,3 @@ render(
     </Provider>,
     document.getElementById('root')
 );
-
-//store.dispatch(getCountries());
